f75204895aa008bc88d12381b47961327600cbd4
장정안 <wjddks96@naver.com>
2024-06-14 17:33:29 +0900

    feat: theme 내 size에 따른 fonts 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,21 +1,37 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 const colors = {
-  black: "#000000",
-  white: "#ffffff",
-  gray01: "#D5D5D5",
-  gray02: "#666666",
+   black: '#000000',
+   white: '#ffffff',
+   gray01: '#D5D5D5',
+   gray02: '#666666',
 };
 
 const fonts = {
-  Title: css`
-    font-family: "Noto Sans";
-    font-size: 2rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 136.241%;
-    letter-spacing: -0.6px;
-  `,
+   Title: css`
+      font-family: 'Noto Sans';
+      font-size: 2rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 136.241%;
+      letter-spacing: -0.6px;
+   `,
+   Small: css`
+      font-family: 'Noto Sans';
+      font-size: 0.9rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 136.241%;
+      letter-spacing: -0.27px;
+   `,
+   large: css`
+      font-family: 'Noto Sans';
+      font-size: 1.2rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 136.241%;
+      letter-spacing: -0.36px;
+   `,
 };
 
 const theme = { colors, fonts };
