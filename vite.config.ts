30505d94197fe9691b73beeac9e1c42cc0e98880
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-06-17 00:27:11 +0900

    [Chore] yarn svgr 세팅 (#19)
    * chore: yarn svgr 세팅
    
    * chore: public/svg 내 svg 파일 추가
    
    * feat: yarn svgr 후 IcArrow 아이콘 생성
    
    * code review: IcArrow svg 파일 하나로 남겨두고 path를 이용해서 색상 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [
-      react({
-         jsxImportSource: '@emotion/react',
-      }),
-   ],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        memo: true,
+      },
+    }),
+  ],
 });
