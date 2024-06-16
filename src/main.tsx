6440d697a6033a26f643a37dadc40490318611a8
jerry <becomegoodguy@gmail.com>
2024-06-17 00:09:30 +0900

    fix: React.StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import Styles from './styles/index.tsx';
@@ -8,12 +7,12 @@ import Styles from './styles/index.tsx';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <QueryClientProvider client={queryClient}>
       <Styles>
         <App />
       </Styles>
       <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </>,
 );
