2bae83271dd1d998858a802100f34b7ee4c18824
장정안 <wjddks96@naver.com>
2024-06-16 20:17:19 +0900

    feat: theme에 as const 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,22 +1,22 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 const colors = {
-  black: "#000000",
-  white: "#ffffff",
-  gray01: "#D5D5D5",
-  gray02: "#666666",
-};
+  black: '#000000',
+  white: '#ffffff',
+  gray01: '#D5D5D5',
+  gray02: '#666666',
+} as const;
 
 const fonts = {
   Title: css`
-    font-family: "Noto Sans";
+    font-family: 'Noto Sans';
     font-size: 2rem;
     font-style: normal;
     font-weight: 700;
     line-height: 136.241%;
     letter-spacing: -0.6px;
   `,
-};
+} as const;
 
 const theme = { colors, fonts };
 
