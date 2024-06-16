3d3172e640237ca8bef60b13a652980488db0516
장정안 <wjddks96@naver.com>
2024-06-17 02:14:20 +0900

    feat: 공통 버튼 컴포넌트에 icon props 추가

--- src/components/@common/button/Button.tsx
+++ src/components/@common/button/Button.tsx
@@ -1,17 +1,20 @@
+import { ReactElement } from 'react';
 import { btnSize, btnStyle, btnVariant } from './Button.style';
 
 interface ButtonProps extends React.HTMLAttributes<HTMLDivElement> {
-   variant?: 'bgWhite' | 'bgBlack';
-   size?: 'small' | 'large';
-   handleBtnClick: () => void;
+  variant?: 'bgWhite' | 'bgBlack';
+  size?: 'small' | 'large';
+  handleBtnClick: () => void;
+  icon?: ReactElement;
 }
 
-const Button = ({ children, variant = 'bgBlack', size = 'large', handleBtnClick }: ButtonProps) => {
-   return (
-      <button css={[btnStyle, btnVariant[variant], btnSize[size]]} onClick={handleBtnClick}>
-         {children}
-      </button>
-   );
+const Button = ({ children, variant = 'bgBlack', size = 'large', handleBtnClick, icon }: ButtonProps) => {
+  return (
+    <button css={[btnStyle, btnVariant[variant], btnSize[size]]} onClick={handleBtnClick}>
+      {children}
+      {icon}
+    </button>
+  );
 };
 
 export default Button;
