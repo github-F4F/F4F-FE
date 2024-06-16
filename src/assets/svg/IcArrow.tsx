8600ac90816a82250edc993c4dee8f23912268cc
장정안 <wjddks96@naver.com>
2024-06-17 02:14:05 +0900

    feat: icArrow svg파일 크기 설정

--- src/assets/svg/IcArrow.tsx
+++ src/assets/svg/IcArrow.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 const SvgIcArrow = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 13 12" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 13 12" width={12} height={12} {...props}>
     <path stroke="#fff" strokeLinecap="square" strokeLinejoin="round" strokeWidth={0.5} d="M4.5 2 8 5.5 4.5 9" />
   </svg>
 );
