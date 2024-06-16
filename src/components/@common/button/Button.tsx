a1b366f45c24e0e04da6dba89cfdafaa6b1cb7e2
장정안 <wjddks96@naver.com>
2024-06-17 03:40:20 +0900

    feat: Html 기본 Div 상속이 아닌 Button 상속으로 변경

--- src/components/@common/button/Button.tsx
+++ src/components/@common/button/Button.tsx
@@ -1,16 +1,16 @@
 import { ReactElement } from 'react';
 import { btnSize, btnStyle, btnVariant } from './Button.style';
 
-interface ButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: 'bgWhite' | 'bgBlack';
   size?: 'small' | 'large';
   handleBtnClick: () => void;
   icon?: ReactElement;
 }
 
-const Button = ({ children, variant = 'bgBlack', size = 'large', handleBtnClick, icon }: ButtonProps) => {
+const Button = ({ children, variant = 'bgBlack', size = 'large', handleBtnClick, icon, ...props }: ButtonProps) => {
   return (
-    <button css={[btnStyle, btnVariant[variant], btnSize[size]]} onClick={handleBtnClick}>
+    <button css={[btnStyle, btnVariant[variant], btnSize[size]]} onClick={handleBtnClick} {...props}>
       {children}
       {icon}
     </button>
