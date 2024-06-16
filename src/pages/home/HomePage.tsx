b164fea9f2744a4e077193212dd179cc9d4dfd6d
장정안 <wjddks96@naver.com>
2024-06-17 04:41:04 +0900

    feat: HomePage 내 fragment 삭제

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,45 +8,43 @@ import Input from '../../components/@common/input/Input';
 
 const HomePage = () => {
   return (
-    <>
-      <main css={HomePageWrapper}>
-        <Bar />
-        <section css={LoginContainer}>
-          <article css={ButtonWrapper}>
-            <Button
-              variant="bgWhite"
-              size="small"
-              handleBtnClick={() => {}}
-              icon={
-                <IcArrow
-                  css={css`
-                    & path {
-                      stroke: ${theme.colors.black};
-                      display: flex;
-                      align-items: center;
-                    }
-                  `}
-                />
-              }
-            >
-              깃허브 토큰 만들러가기
-            </Button>
-            <div css={SubTextWrapper}>
-              <p css={SubText}>토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!</p>
-            </div>
-          </article>
-          <article css={ButtonWrapper}>
-            <Input variant="default" size="large" placeholder="GitHub 토큰을 입력해주세요" />
-          </article>
-          <article css={ButtonWrapper}>
-            <Button variant="bgBlack" size="large" handleBtnClick={() => {}}>
-              나의 맞팔 확인하기
-            </Button>
-          </article>
-        </section>
-        <Bar />
-      </main>
-    </>
+    <main css={HomePageWrapper}>
+      <Bar />
+      <section css={LoginContainer}>
+        <article css={ButtonWrapper}>
+          <Button
+            variant="bgWhite"
+            size="small"
+            handleBtnClick={() => {}}
+            icon={
+              <IcArrow
+                css={css`
+                  & path {
+                    stroke: ${theme.colors.black};
+                    display: flex;
+                    align-items: center;
+                  }
+                `}
+              />
+            }
+          >
+            깃허브 토큰 만들러가기
+          </Button>
+          <div css={SubTextWrapper}>
+            <p css={SubText}>토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!</p>
+          </div>
+        </article>
+        <article css={ButtonWrapper}>
+          <Input variant="default" size="large" placeholder="GitHub 토큰을 입력해주세요" />
+        </article>
+        <article css={ButtonWrapper}>
+          <Button variant="bgBlack" size="large" handleBtnClick={() => {}}>
+            나의 맞팔 확인하기
+          </Button>
+        </article>
+      </section>
+      <Bar />
+    </main>
   );
 };
 
