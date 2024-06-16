b7b6f3c2c0eb335d54df32d6339f686437f7b5ac
장정안 <wjddks96@naver.com>
2024-06-16 21:44:15 +0900

    feat: 공통 버튼 컴포넌트 스타일 코드 추가

--- src/components/@common/button/Button.style.ts
+++ src/components/@common/button/Button.style.ts
@@ -2,38 +2,39 @@ import { css } from '@emotion/react';
 import theme from '../../../styles/theme';
 
 export const btnStyle = css`
-   border-radius: 30px;
-   border: none;
-   display: flex;
-   padding-left: 1rem;
-   justify-content: center;
-   align-items: center;
-   ${theme.fonts.Title};
+  border-radius: 30px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${theme.fonts.Title};
+  border-radius: 30px;
+  border: 1px solid #d5d5d5;
 
-   cursor: pointer;
+  cursor: pointer;
 `;
 
 export const btnVariant = {
-   bgWhite: css`
-      color: ${theme.colors.gray02};
-      background-color: ${theme.colors.white};
-   `,
-   bgBlack: css`
-      color: ${theme.colors.white};
-      background-color: ${theme.colors.black};
-   `,
+  bgWhite: css`
+    color: ${theme.colors.gray02};
+    background-color: ${theme.colors.white};
+  `,
+  bgBlack: css`
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.black};
+  `,
 };
 
 export const btnSize = {
-   small: css`
-      width: 12.9rem;
-      height: 2.8rem;
-      ${theme.fonts.Small};
-   `,
-   large: css`
-      width: 39.9rem;
-      height: 4.5rem;
-      padding-left: 1rem;
-      ${theme.fonts.large}
-   `,
+  small: css`
+    width: 12.9rem;
+    height: 2.8rem;
+    ${theme.fonts.Detail};
+  `,
+  large: css`
+    width: 39.9rem;
+    height: 4.5rem;
+    padding-left: 1rem;
+    ${theme.fonts.Title}
+  `,
 };
