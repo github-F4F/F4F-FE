df1966b3e3280be4a848d97537050b7803a45ba6
장정안 <wjddks96@naver.com>
2024-06-16 22:18:05 +0900

    feat: 홈페이지 버튼을 제외한 뷰 구현

---
+++ src/pages/home/HomePage.style.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+import theme from '../../styles/theme';
+
+export const HomePageWrapper = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100vh',
+});
+
+export const LoginContainer = css({
+  display: 'flex',
+  width: '43.2rem',
+  height: '20rem',
+  borderRadius: '1.2rem',
+  marginTop: '4.5rem',
+  marginBottom: '4.5rem',
+  border: `0.1rem solid ${theme.colors.gray01}`,
+  background: `${theme.colors.white}`,
+});
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,29 @@
+import Bar from '../../components/bar/Bar';
+import { HomePageWrapper, LoginContainer } from './HomePage.style';
+import Button from '../../components/@common/button/Button';
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <>
+      <main css={HomePageWrapper}>
+        <Bar />
+        <section css={LoginContainer}>
+          <div
+            css={{
+              display: 'flex',
+              marginLeft: '1.3rem',
+              marginTop: '1rem',
+            }}
+          >
+            <Button variant="bgWhite" size="small">
+              깃허브 토큰 만들러가기
+            </Button>
+          </div>
+        </section>
+        <Bar />
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
