223b3baaceba28be583d12511062af215efa0f30
jerry <becomegoodguy@gmail.com>
2024-06-17 00:09:07 +0900

    chore: react-query 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import Styles from "./styles/index.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import Styles from './styles/index.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Styles>
-      <App />
-    </Styles>
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Styles>
+        <App />
+      </Styles>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
