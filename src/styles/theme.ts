5e651b44f026c1f1c0b0d80e33866c260d4d608d
jerry <becomegoodguy@gmail.com>
2024-06-16 22:21:16 +0900

    style: gmarket 폰트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,30 @@ const fonts = {
     line-height: 136.241%;
     letter-spacing: -0.6px;
   `,
+  body01: css`
+    font-family: 'GmarketSans';
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 100.03%; /* 16.005px */
+    letter-spacing: -1.28px;
+  `,
+  body02: css`
+    font-family: 'GmarketSans';
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 136.241%; /* 16.349px */
+    letter-spacing: -0.36px;
+  `,
+  body03: css`
+    font-family: 'GmarketSans';
+    font-size: 1%.2;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 100.03%; /* 16.005px */
+    letter-spacing: -1.28px;
+  `,
 } as const;
 
 const theme = { colors, fonts };
