85c63bd72979a8c4ec83ea1058b5b1ea8a902c84
장정안 <wjddks96@naver.com>
2024-06-17 00:29:01 +0900

    feat: 버튼에 handleBtnClick 속성 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
+import Button from '../../components/@common/button/Button';
 import Bar from '../../components/bar/Bar';
 import { HomePageWrapper, LoginContainer } from './HomePage.style';
-import Button from '../../components/@common/button/Button';
 
 const HomePage = () => {
   return (
@@ -15,7 +15,7 @@ const HomePage = () => {
               marginTop: '1rem',
             }}
           >
-            <Button variant="bgWhite" size="small">
+            <Button variant="bgWhite" size="small" handleBtnClick={() => {}}>
               깃허브 토큰 만들러가기
             </Button>
           </div>
