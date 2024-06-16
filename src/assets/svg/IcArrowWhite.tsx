7445e44dbd5dd6bc530a696a9a9f4c329d2078d4
장정안 <wjddks96@naver.com>
2024-06-16 22:10:00 +0900

    feat: yarn svgr 후 IcArrow 아이콘 생성

---
+++ src/assets/svg/IcArrowWhite.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+const SvgIcArrowWhite = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 13 12" {...props}>
+    <path stroke="#fff" strokeLinecap="square" strokeLinejoin="round" strokeWidth={0.5} d="M4.5 2 8 5.5 4.5 9" />
+  </svg>
+);
+export default SvgIcArrowWhite;
