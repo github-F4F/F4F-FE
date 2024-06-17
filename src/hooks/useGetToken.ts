6c7cd5e65d84011381ea863a01b8e69f8664aaca
장정안 <wjddks96@naver.com>
2024-06-17 15:47:07 +0900

    feat: 토큰값 입력 로직 구현

--- src/hooks/useGetToken.ts
+++ src/hooks/useGetToken.ts
@@ -1,20 +1,30 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useGetUser } from './queries/useGetUser';
 
 const useGetToken = () => {
+  const navigate = useNavigate();
   const [token, setToken] = useState('');
+  const { data: userInfo } = useGetUser(token);
 
   const handleChangeToken = (e: React.ChangeEvent<HTMLInputElement>) => {
     setToken(e.target.value);
   };
 
-  const handleClickCheckFollwer = () => {
-    // router.push('/follow-list');
-    sessionStorage.setItem('token', token);
+  const handleClickCheckFollower = () => {
+    if (userInfo) {
+      localStorage.setItem('token', token);
+      navigate('/search');
+    } else {
+      alert('토큰이 없음!');
+      console.error('토큰이 없음!');
+    }
   };
 
   return {
     token,
-    handleChangeTokenInput,
+    handleChangeToken,
+    handleClickCheckFollower,
   };
 };
 
