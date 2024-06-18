05a6c5f7a44999bfa2ed7ed0f10866b12dd73b8a
jerry <becomegoodguy@gmail.com>
2024-06-18 19:25:36 +0900

    feat: 팔로잉 리스트 뷰 추가

---
+++ src/pages/search/component/CardList.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import Loading from '../../../components/@common/loading/Loading';
+import { useGetFollowList } from '../../../hooks/queries/useGetFollowList';
+import Card from './Card';
+
+const CardList = () => {
+  const user_token = localStorage.getItem('token') || '';
+
+  const { ref, inView } = useInView();
+  const { data, fetchNextPage, isFetchingNextPage } = useGetFollowList(user_token);
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
+  return (
+    <div
+      css={{ height: '500px', overflow: 'auto', borderRadius: '12px', border: '1px solid #D5D5D5', padding: '0 1rem' }}
+    >
+      {data?.pages[data?.pages.length - 1].map((user, userIndex) => (
+        <Card key={`user-${userIndex}`} {...user} />
+      ))}
+      {isFetchingNextPage ? (
+        <div css={{ width: '27rem', marginTop: '5rem' }}>
+          <Loading />
+        </div>
+      ) : (
+        <div ref={ref} />
+      )}
+    </div>
+  );
+};
+
+export default CardList;
