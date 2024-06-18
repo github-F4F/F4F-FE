e70c0d3d00346d70611f78635a8c95546afb7e08
장정안 <wjddks96@naver.com>
2024-06-18 17:25:48 +0900

    feat: 팔로워 데이터 가져오는 axios 구현

--- src/apis/getUser.ts
+++ src/apis/getUser.ts
@@ -1,7 +1,15 @@
+import { User } from '../types/user';
 import { Client } from './client';
+
+const PER_PAGE = 100;
 
 export const getUserInfo = async (token: string) => {
   const { data } = await Client(token).get(`/user`);
 
+  return data;
+};
+
+export const getFollowersList = async (token: string): Promise<User[]> => {
+  const { data } = await Client(token).get<User[]>(`/user/followers?per_page=${PER_PAGE}`);
   return data;
 };
