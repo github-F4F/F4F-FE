fe529d236608a5542993b706bdd21a2a708373af
장정안 <wjddks96@naver.com>
2024-06-19 18:37:21 +0900

    feat: FollowerListBox 컴포넌트 내 무한스크롤 기능 구현

--- src/pages/search/component/followerLIstBox/FollowerListBox.tsx
+++ src/pages/search/component/followerLIstBox/FollowerListBox.tsx
@@ -1,10 +1,20 @@
 import { FollowerListWrapper } from './FollowerListBox.style';
-import { useGetFollowersList } from '../../../../hooks/queries/useGetUser';
 import Loading from '../../../../components/@common/loading/Loading';
 import FollowerLists from '../followerLists/FollowerLists';
+import useFollowerInfiniteQuery from '../../../../hooks/queries/useFollowerInfiniteQuery';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
 const FollowerListBox = () => {
   const token = localStorage.getItem('token') || '';
-  const { data, isLoading } = useGetFollowersList(token);
+  const { data, isLoading, fetchNextPage, isFetchingNextPage } = useFollowerInfiniteQuery(token);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
 
   return (
     <>
@@ -13,9 +23,16 @@ const FollowerListBox = () => {
       ) : (
         <>
           <section css={FollowerListWrapper}>
-            {data?.map(user => (
-              <FollowerLists key={user.id} data={[user]} />
+            {data?.pages[data?.pages.length - 1].map(user => (
+              <FollowerLists key={user.id} {...user} />
             ))}
+            {isFetchingNextPage ? (
+              <div css={{ width: '27rem', marginTop: '5rem' }}>
+                <Loading />
+              </div>
+            ) : (
+              <div ref={ref} />
+            )}
           </section>
         </>
       )}
