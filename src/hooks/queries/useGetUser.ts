7645b32ab4069ed978690ed2f4385e3127ba3617
장정안 <wjddks96@naver.com>
2024-06-19 18:32:50 +0900

    delete: 무한스크롤 구현으로 인한 팔로잉 유저 호출하는 함수 삭제

--- src/hooks/queries/useGetUser.ts
+++ src/hooks/queries/useGetUser.ts
@@ -1,22 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { getFollowersList, getUserInfo } from '../../apis/getUser';
+import { getUserInfo } from '../../apis/getUser';
 import { USER_QUERY_KEY } from './key';
 
 export const useGetUser = (token: string) => {
   const { data, isLoading, error } = useQuery({
     queryKey: [USER_QUERY_KEY.userInfo],
     queryFn: () => getUserInfo(token),
-    staleTime: 50000,
-    gcTime: 100000,
-  });
-
-  return { data, isLoading, error };
-};
-
-export const useGetFollowersList = (token: string) => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: [USER_QUERY_KEY.follower_list],
-    queryFn: () => getFollowersList(token),
     staleTime: 50000,
     gcTime: 100000,
   });
